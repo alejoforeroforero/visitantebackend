b459a8613379f87f936fcb080e37d608999e78f9
alejoforeroforero <alejoforeroforero@gmail.com>
2024-02-07 15:13:13 -0500

    second

--- server.js
+++ server.js
@@ -8,30 +8,35 @@ const grabacionesRouter = require("./routes/grabaciones");
 
 const app = express();
 
-
 // Middlewares
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 //Routes
 
-app.get('/', (req, res)=>{
-    res.send("Home Page")
-})
-app.use("/admin/grabaciones/", grabacionesRouter);
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+// app.use("/admin/grabaciones/", grabacionesRouter);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(process.env.DATABASE_URL)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// const PORT = process.env.PORT || 5000;
+
+// mongoose
+//   .connect(process.env.DATABASE_URL)
+//   .then(() => {
+//     app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
